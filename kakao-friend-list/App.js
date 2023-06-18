fcfe6a2caa7caf968a529c92911d62cf167304a8
ricky <kimzzan1234@gmail.com>
2023-06-18 17:32:23 +0900

    kakao 클론코딩

--- kakao-friend-list/App.js
+++ kakao-friend-list/App.js
@@ -1,34 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Header from './src/Header';
-import { getStatusBarHeight } from 'react-native-iphone-x-helper';
-import MyProfile from './src/MyProfile';
-import { myProfile } from './src/data';
-import Margin from './src/Margin';
-// import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { useState } from "react";
+import { FlatList, StyleSheet, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
-const statusBarHeight = getStatusBarHeight(true);
+import { friendProfiles, myProfile } from "./src/data";
+import Division from "./src/Division";
+import FriendList from "./src/FriendList";
+import FriendSection from "./src/FriendSection";
+import Header from "./src/Header";
+import Margin from "./src/Margin";
+import Profile from "./src/Profile";
+import TabBar from "./src/TabBar";
 
 export default function App() {
-  return (
-    // <SafeAreaProvider>
-    //   <SafeAreaView 
-    //     style={styles.container} 
-    //     edges={['right', 'left']} // 예외없이 모두 안전영역 적용
-    //   >
-    //     <Header />
-    //   </SafeAreaView>
-    // </SafeAreaProvider>
-    <View style={styles.container}>
-      <Header />
+  const [isOpened, setIsOpened] = useState(true);
+  const [selectedTabIdx, setSelectedTabIdx] = useState(0);
 
-      <Margin height={10} />
+  const onPressArrow = () => {
+    setIsOpened(!isOpened);
+  }
 
-      <MyProfile 
-        uri={myProfile.uri}
-        name={myProfile.name}
-        introduction={myProfile.introduction}
+  const ItemSeparatorComponent = () => <Margin height={13} />
+  const renderItem = ({ item }) => (
+    <View>
+      <Profile
+        uri={item.uri}
+        name={item.name} 
+        introduction={item.introduction}
+        isMe={false}
       />
+    </View>
+  )
+  const ListHeaderComponent = () => (
+    <View style={{ backgroundColor:"white" }}>
+      <Header />
+        
+        <Margin height={10} />
+
+        <Profile
+          uri={myProfile.uri}
+          name={myProfile.name} 
+          introduction={myProfile.introduction}
+          isMe={true}
+        />
+
+        <Margin height={15} />
+
+        <Division />
+
+        <Margin height={12} />
+
+        <FriendSection 
+          friendProfileLen={friendProfiles.length} 
+          onPressArrow={onPressArrow}
+          isOpened={isOpened}
+        />
+
+        <Margin height={5} />
+    </View>
+  )
+  const ListFooterComponent = () => <Margin height={10} />
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView
+        style={styles.container}
+        edges={['top', 'right', 'bottom', 'left']} // 예외없이 모두 안전영역 적용
+      >
+        <FlatList
+          data={isOpened ? friendProfiles : []}
+          contentContainerStyle={{ paddingHorizontal: 15 }}
+          keyExtractor={(_, index) => index}
+          stickyHeaderIndices={[0]}
+          ItemSeparatorComponent={ItemSeparatorComponent}
+          renderItem={renderItem}
+          ListHeaderComponent={ListHeaderComponent}
+          ListFooterComponent={ListFooterComponent}
+          showsVerticalScrollIndicator={false}
+        />
+        <TabBar selectedTabIdx={selectedTabIdx} setSelectedTabIdx={setSelectedTabIdx} />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  )
+
+  return (
+    <View style={styles.container}>
+      <View style={{
+          flex: 1,
+          paddingHorizontal: 15,
+      }}>
+        <FriendList data={friendProfiles} isOpened={isOpened} />
+      </View>
+
     </View>
   );
 }
@@ -36,7 +98,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: statusBarHeight,
+    backgroundColor: "#fff",
   },
 });
